5fcdd958b293b7a1cf86299eff61fe66bafbc725
Myname <assa123assa1234@gmail.com>
2026-05-21 19:52:27 +0400

    проверяю работу git

--- hello_world/src/hello_world.java
+++ hello_world/src/hello_world.java
@@ -5,9 +5,10 @@ public class hello_world {
     public static void main (String[] args) {
         int a = 10;
         int b = 20;
+        int c = 30;
         System.out.println("hello world");
-        System.out.println(a + b);
+        System.out.println(a + b - c);
     }
-
+0
 
 }
