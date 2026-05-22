ceaf1dc0fa50dfcbd102f1ae0658ee5d8fbcf5fe
Myname <assa123assa1234@gmail.com>
2026-05-22 20:11:59 +0400

    сканер

--- hello_world/src/scanner.java
+++ hello_world/src/scanner.java
@@ -3,19 +3,40 @@ import java.util.Scanner;
 public class scanner {
 
     public static void main(String[] args) {
-
+        String name;
+        int age;
         Scanner console = new Scanner(System.in);
-        System.out.println("Пожалуйста введите ваше имя :");
-        if(console.hasNextInt()){
-            System.out.println("Ошибка ! Пожалуйста введите имя");
+        while (true) {
+            System.out.println("Введите пожалуйста имя: ");
+            if (console.hasNextInt()) {
+                System.out.println("Ошибка! Попробуйте еще раз");
+                console.nextLine();
+            }
+            else {
+                name = console.nextLine();
+                System.out.println("Спасибо, " + name.toUpperCase() + "\n" + "Пожалуйста укажите возраст: ");
+                break;
+            }
         }
-        else{
-            String name = console.nextLine();
-            System.out.println("Спасибо, " + name);
+        while (true) {
+            if (console.hasNextInt()) {
+                age = console.nextInt();
+                System.out.println("Ваше имя: " +"[" + name.toUpperCase() +"]");
+                System.out.println("Ваш возраст: " + "[" + age + "]");
+                break;
+            }
+            else {
+                System.out.println("Ошибка! Попробуйте еще раз");
+                console.next();
+            }
+
+
         }
-        console.close();
+
+        }
+
 
 
 
     }
-}
+
